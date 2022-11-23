301dfddcda9a7992ecd76cd7518ecc2923eeaa0d
jeayoung <k01066624566@gmail.com>
2022-11-24 07:59:19 +0900

    jwt 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,9 @@ dependencies {
 	runtimeOnly("org.mariadb.jdbc:mariadb-java-client")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.security:spring-security-test")
+	implementation("io.jsonwebtoken:jjwt-api:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
+	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.11.5")
 }
 
 tasks.withType<KotlinCompile> {
