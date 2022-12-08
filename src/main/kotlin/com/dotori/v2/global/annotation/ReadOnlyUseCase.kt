9df13ed821ebe1c6d1717c83ff17822ef9088ae1
Huuuunee <s21024@gsm.hs.kr>
2022-12-08 10:55:59 +0900

    [DELETE] 사용하지 않는 어노테이션 삭제

--- src/main/kotlin/com/dotori/v2/global/annotation/ReadOnlyUseCase.kt
+++
@@ -1,8 +0,0 @@
-package com.dotori.v2.global.annotation
-
-import org.springframework.stereotype.Service
-import org.springframework.transaction.annotation.Transactional
-
-@Service
-@Transactional(rollbackFor = [Exception::class], readOnly = true)
-annotation class ReadOnlyUseCase()
