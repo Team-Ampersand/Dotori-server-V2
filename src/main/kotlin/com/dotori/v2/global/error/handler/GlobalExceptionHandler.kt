33565204c93f4664ec0336d101481d6e35520668
Huuuunee <s21024@gsm.hs.kr>
2023-01-17 04:05:36 +0900

    :sparkles: GlobalExceptionHandler 생성

---
+++ src/main/kotlin/com/dotori/v2/global/error/handler/GlobalExceptionHandler.kt
@@ -0,0 +1,21 @@
+package com.dotori.v2.global.error.handler
+
+import com.dotori.v2.global.error.ErrorResponse
+import com.dotori.v2.global.error.exception.BasicException
+import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.servlet.http.HttpServletRequest
+
+class GlobalExceptionHandler {
+    private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
+
+    @ExceptionHandler(BasicException::class)
+    fun basicExceptionHandler(request: HttpServletRequest, ex: BasicException): ResponseEntity<ErrorResponse> {
+        log?.error(request.requestURI)
+        log?.error(ex.message)
+        val errorResponse = ErrorResponse(ex.errorCode)
+        return ResponseEntity<ErrorResponse>(errorResponse, HttpStatus.valueOf(ex.errorCode.code))
+    }
+}
