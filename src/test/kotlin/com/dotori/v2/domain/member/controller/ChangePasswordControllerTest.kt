4e607b6f3695d3b515cf5bfc2f20894ef500db7c
jeayoung <k01066624566@gmail.com>
2023-02-15 16:09:44 +0900

    add TestCode

--- src/test/kotlin/com/dotori/v2/domain/member/controller/ChangePasswordControllerTest.kt
+++ src/test/kotlin/com/dotori/v2/domain/member/controller/ChangePasswordControllerTest.kt
@@ -1,10 +1,11 @@
 package com.dotori.v2.domain.member.controller
 
-import com.dotori.v2.domain.member.presentation.AuthController
 import com.dotori.v2.domain.member.presentation.MemberController
-import com.dotori.v2.domain.member.presentation.dto.req.WithdrawalReqDto
+import com.dotori.v2.domain.member.presentation.dto.req.NewPasswordReqDto
 import com.dotori.v2.domain.member.presentation.dto.res.LogoutResDto
-import com.dotori.v2.domain.member.service.*
+import com.dotori.v2.domain.member.service.ChangePasswordService
+import com.dotori.v2.domain.member.service.LogoutService
+import com.dotori.v2.domain.member.service.WithdrawalService
 import io.kotest.core.spec.style.BehaviorSpec
 import io.kotest.matchers.shouldBe
 import io.mockk.every
@@ -12,7 +13,7 @@ import io.mockk.mockk
 import io.mockk.verify
 import org.springframework.http.HttpStatus
 
-class WithdrawalControllerTest : BehaviorSpec({
+class ChangePasswordControllerTest : BehaviorSpec({
     val logoutService = mockk<LogoutService>()
     val withdrawalService = mockk<WithdrawalService>()
     val changePasswordService = mockk<ChangePasswordService>()
@@ -20,11 +21,11 @@ class WithdrawalControllerTest : BehaviorSpec({
 
     given("요청이 들어오면") {
         `when`("is received") {
-            val reqDto = WithdrawalReqDto("test@gsm.hs.kr", "1234")
-            every { withdrawalService.execute(reqDto) } returns Unit
-            val response = authController.withdrawal(reqDto)
+            val request = NewPasswordReqDto("", "")
+            every { changePasswordService.execute(request) } returns Unit
+            val response = authController.changePassword(request)
             then("서비스가 한번은 실행되어야 함") {
-                verify(exactly = 1) { withdrawalService.execute(reqDto) }
+                verify(exactly = 1) { changePasswordService.execute(request) }
             }
             then("response status should be ok") {
                 response.statusCode shouldBe HttpStatus.OK
