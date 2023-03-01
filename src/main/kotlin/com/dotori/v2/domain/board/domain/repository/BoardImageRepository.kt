0015c313d98468d9681f2b501aeb079e54ed1559
Huuuunee <s21024@gsm.hs.kr>
2023-03-02 01:24:15 +0900

    :sparkles: 사진을 찾아오는 함수 생성

--- src/main/kotlin/com/dotori/v2/domain/board/domain/repository/BoardImageRepository.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/domain/repository/BoardImageRepository.kt
@@ -4,4 +4,5 @@ import com.dotori.v2.domain.board.domain.entity.BoardImage
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface BoardImageRepository : JpaRepository<BoardImage, Long>{
+    fun findAllByBoard_Id(boardId: Long): List<BoardImage>?
 }
