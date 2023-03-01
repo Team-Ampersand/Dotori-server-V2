fbd1b12c9923f5413e73b6f69d941d76d1e419c6
Huuuunee <s21024@gsm.hs.kr>
2023-03-01 22:56:38 +0900

    :sparkles: 컨트롤러 설정

--- src/main/kotlin/com/dotori/v2/domain/board/presentation/admin/AdminBoardController.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/presentation/admin/AdminBoardController.kt
@@ -1,20 +1,20 @@
 package com.dotori.v2.domain.board.presentation.admin
 
 import com.dotori.v2.domain.board.presentation.data.req.CreateBoardReqDto
+import com.dotori.v2.domain.board.presentation.data.req.ModifyBoardReqDto
 import com.dotori.v2.domain.board.service.CreateBoardService
+import com.dotori.v2.domain.board.service.ModifyBoardService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestPart
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
 import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v2/admin/board")
 class AdminBoardController(
-    private val createBoardService: CreateBoardService
+    private val createBoardService: CreateBoardService,
+    private val modifyBoardService: ModifyBoardService
 ) {
 
     @PostMapping
@@ -24,5 +24,13 @@ class AdminBoardController(
     ): ResponseEntity<Void> =
         createBoardService.execute(createBoardReqDto, multipartFiles)
             .run { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @PutMapping("/{board_id}")
+    fun modifyBoard(
+        @Valid @RequestBody modifyBoardReqDto: ModifyBoardReqDto,
+        @PathVariable board_id: Long
+    ): ResponseEntity<Void> =
+        modifyBoardService.execute(modifyBoardReqDto, board_id)
+            .run { ResponseEntity.status(HttpStatus.OK).build() }
 
 }
