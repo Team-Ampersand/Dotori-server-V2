1425fc926ef01367585088b446bfd8385d730abf
Huuuunee <s21024@gsm.hs.kr>
2023-03-02 20:23:40 +0900

    :sparkles: 컨트롤러 매핑

---
+++ src/main/kotlin/com/dotori/v2/domain/music/presentation/councillor/CouncillorMusicController.kt
@@ -0,0 +1,22 @@
+package com.dotori.v2.domain.music.presentation.councillor
+
+import com.dotori.v2.domain.music.presentation.data.req.ApplyMusicReqDto
+import com.dotori.v2.domain.music.service.ApplyMusicService
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDateTime
+
+@RestController
+@RequestMapping("/v2/councillor/music")
+class CouncillorMusicController(
+    private val applyMusicService: ApplyMusicService
+) {
+    @PostMapping
+    fun applyMusic(@RequestBody applyMusicReqDto: ApplyMusicReqDto): ResponseEntity<Void> =
+        applyMusicService.execute(applyMusicReqDto, LocalDateTime.now().dayOfWeek)
+            .run { ResponseEntity.status(HttpStatus.CREATED).build() }
+}
