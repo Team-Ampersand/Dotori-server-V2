2f1062c459a904fd1ec2908328faa4136ff30e8f
Huuuunee <s21024@gsm.hs.kr>
2023-03-02 13:50:20 +0900

    :recycle: pageable 적용

--- src/main/kotlin/com/dotori/v2/domain/board/presentation/member/MemberBoardController.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/presentation/member/MemberBoardController.kt
@@ -2,6 +2,8 @@ package com.dotori.v2.domain.board.presentation.member
 
 import com.dotori.v2.domain.board.presentation.data.res.ListBoardResDto
 import com.dotori.v2.domain.board.service.GetBoardsService
+import org.springframework.data.domain.Pageable
+import org.springframework.data.web.PageableDefault
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -14,6 +16,6 @@ class MemberBoardController(
     private val getBoardsService: GetBoardsService
 ) {
     @GetMapping
-    fun findBoards(): ResponseEntity<ListBoardResDto> =
+    fun findBoards(@PageableDefault(size = 6) pageable: Pageable): ResponseEntity<ListBoardResDto> =
         ResponseEntity.status(HttpStatus.OK).body(getBoardsService.execute())
 }
