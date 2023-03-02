83d96bf6c0f661dffa2838d1a86233ea6068c5a7
Huuuunee <s21024@gsm.hs.kr>
2023-03-02 14:49:13 +0900

    :recycle: 공지사항 작성자의 role을 반환하도록 변경

--- src/main/kotlin/com/dotori/v2/domain/board/service/impl/GetBoardsServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/service/impl/GetBoardsServiceImpl.kt
@@ -5,8 +5,6 @@ import com.dotori.v2.domain.board.domain.repository.BoardRepository
 import com.dotori.v2.domain.board.presentation.data.res.BoardResDto
 import com.dotori.v2.domain.board.presentation.data.res.ListBoardResDto
 import com.dotori.v2.domain.board.service.GetBoardsService
-import com.dotori.v2.domain.member.domain.entity.Member
-import com.dotori.v2.global.util.UserUtil
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,21 +12,19 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(readOnly = true, rollbackFor = [Exception::class])
 class GetBoardsServiceImpl(
     private val boardRepository: BoardRepository,
-    private val userUtil: UserUtil
 ) : GetBoardsService {
     override fun execute(): ListBoardResDto {
-        val memberInfo: Member = userUtil.fetchCurrentUser()
         return ListBoardResDto(
             content = boardRepository.findAll()
-                .map { toDto(it, memberInfo) }
+                .map { toDto(it) }
         )
     }
 
-    private fun toDto(board: Board, member: Member): BoardResDto =
+    private fun toDto(board: Board): BoardResDto =
         BoardResDto(
             id = board.id,
             title = board.title,
-            roles = member.roles,
+            roles = board.member.roles,
             createdDate = board.createdDate
         )
 
