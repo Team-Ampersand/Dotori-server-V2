6887251815293be68379304493991af82af37d09
Huuuunee <s21024@gsm.hs.kr>
2023-03-02 20:25:44 +0900

    :sparkles: 유저 음악신청 상태를 CAN으로 변경하는 함수 생성

--- src/main/kotlin/com/dotori/v2/domain/music/domain/repository/MusicRepository.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/domain/repository/MusicRepository.kt
@@ -2,6 +2,9 @@ package com.dotori.v2.domain.music.domain.repository
 
 import com.dotori.v2.domain.music.domain.entity.Music
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 
 interface MusicRepository : JpaRepository<Music, Long> {
+    @Query("update Member member set member.musicStatus = 'CAN'")
+    fun updateMusicStatusMemberByMember()
 }
