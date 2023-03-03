8a570ec49bd1452a156cdffa474fb3a61e6d5348
Huuuunee <s21024@gsm.hs.kr>
2023-03-03 11:07:53 +0900

    :recycle: 컨트롤러 적용을 안했던 이슈

--- src/main/kotlin/com/dotori/v2/domain/music/presentation/councillor/CouncillorMusicController.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/presentation/councillor/CouncillorMusicController.kt
@@ -1,22 +1,33 @@
 package com.dotori.v2.domain.music.presentation.councillor
 
 import com.dotori.v2.domain.music.presentation.data.req.ApplyMusicReqDto
+import com.dotori.v2.domain.music.presentation.data.res.MusicListResDto
 import com.dotori.v2.domain.music.service.ApplyMusicService
+import com.dotori.v2.domain.music.service.FindMusicsService
+import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
+import java.time.LocalDate
 import java.time.LocalDateTime
 
 @RestController
 @RequestMapping("/v2/councillor/music")
 class CouncillorMusicController(
-    private val applyMusicService: ApplyMusicService
+    private val applyMusicService: ApplyMusicService,
+    private val findMusicsService: FindMusicsService
 ) {
     @PostMapping
     fun applyMusic(@RequestBody applyMusicReqDto: ApplyMusicReqDto): ResponseEntity<Void> =
         applyMusicService.execute(applyMusicReqDto, LocalDateTime.now().dayOfWeek)
             .run { ResponseEntity.status(HttpStatus.CREATED).build() }
+
+    @GetMapping
+    fun findMusics(
+        @RequestParam(
+            value = "date",
+            required = true
+        ) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate
+    ): ResponseEntity<MusicListResDto> =
+        ResponseEntity.status(HttpStatus.OK).body(findMusicsService.execute(date))
 }
