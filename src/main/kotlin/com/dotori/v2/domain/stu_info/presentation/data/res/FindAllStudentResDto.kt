4e50e5b8ddf3ee45da2cdd5c07e9cf67a2afae31
전승원 <78971821+louis7308@users.noreply.github.com>
2023-03-10 10:46:39 +0900

    :ambulance: :: 학생정보 전체 조회 로직 리팩토링

--- src/main/kotlin/com/dotori/v2/domain/stu_info/presentation/data/res/FindAllStudentResDto.kt
+++ src/main/kotlin/com/dotori/v2/domain/stu_info/presentation/data/res/FindAllStudentResDto.kt
@@ -2,6 +2,7 @@ package com.dotori.v2.domain.stu_info.presentation.data.res
 
 import com.dotori.v2.domain.member.enums.Gender
 import com.dotori.v2.domain.member.enums.Role
+import com.dotori.v2.domain.member.enums.SelfStudyStatus
 
 data class FindAllStudentResDto(
     val id: Long,
@@ -9,5 +10,5 @@ data class FindAllStudentResDto(
     val memberName: String,
     val stuNum: String,
     val role: Role,
-    val selfStudyCheck: Boolean
+    val selfStudyStatus: SelfStudyStatus
 )
