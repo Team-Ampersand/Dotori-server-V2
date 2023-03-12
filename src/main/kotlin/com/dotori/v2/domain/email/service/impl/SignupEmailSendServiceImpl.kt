555cb5dbffab49f463b46b17b9ea5bb4985eb78f
jeayoung <k01066624566@gmail.com>
2023-03-13 08:08:06 +0900

    오타 수정

--- src/main/kotlin/com/dotori/v2/domain/email/service/impl/SignupEmailSendServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/email/service/impl/SignupEmailSendServiceImpl.kt
@@ -2,16 +2,16 @@ package com.dotori.v2.domain.email.service.impl
 
 import com.dotori.v2.domain.email.presentation.dto.request.EmailReqDto
 import com.dotori.v2.domain.email.service.EmailSendService
-import com.dotori.v2.domain.email.service.SingnupEmailSendService
+import com.dotori.v2.domain.email.service.SignupEmailSendService
 import com.dotori.v2.domain.member.domain.repository.MemberRepository
 import com.dotori.v2.domain.member.exception.MemberAlreadyException
 import org.springframework.stereotype.Service
 
 @Service
-class SingnupEmailSendServiceImpl(
+class SignupEmailSendServiceImpl(
     private val emailSendService: EmailSendService,
     private val memberRepository: MemberRepository
-) : SingnupEmailSendService {
+) : SignupEmailSendService {
     override fun execute(emailReqDto: EmailReqDto): String {
         if (memberRepository.existsByEmail(emailReqDto.email))
             throw MemberAlreadyException()
