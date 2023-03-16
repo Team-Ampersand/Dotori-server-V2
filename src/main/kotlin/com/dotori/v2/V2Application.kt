927940804a9823c57cc5b0e9435848282a795540
jeayoung <k01066624566@gmail.com>
2023-03-16 20:37:26 +0900

    timeZone세팅

--- src/main/kotlin/com/dotori/v2/V2Application.kt
+++ src/main/kotlin/com/dotori/v2/V2Application.kt
@@ -1,12 +1,22 @@
 package com.dotori.v2
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.scheduling.annotation.EnableScheduling
+import java.util.*
+import javax.annotation.PostConstruct
 
 @EnableScheduling
 @SpringBootApplication
-class V2Application
+class V2Application{
+	val log = LoggerFactory.getLogger(this::class.java.name)!!
+	@PostConstruct
+	fun timeZone() {
+		TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"))
+		log.info("Server Started At {}", Date())
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<V2Application>(*args)
