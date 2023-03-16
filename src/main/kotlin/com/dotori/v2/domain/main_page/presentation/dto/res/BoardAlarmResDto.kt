c86a671b7d5c9834aeb5f09158840f4e1be831af
Huuuunee <s21024@gsm.hs.kr>
2023-03-16 11:29:33 +0900

    :recycle: 공지사항 리스트로 변경

--- src/main/kotlin/com/dotori/v2/domain/main_page/presentation/dto/res/BoardAlarmResDto.kt
+++ src/main/kotlin/com/dotori/v2/domain/main_page/presentation/dto/res/BoardAlarmResDto.kt
@@ -1,14 +1,11 @@
 package com.dotori.v2.domain.main_page.presentation.dto.res
 
 import com.dotori.v2.domain.board.domain.entity.Board
+import com.dotori.v2.domain.board.presentation.data.dto.BoardDto
+import com.dotori.v2.domain.board.presentation.data.res.BoardResDto
 import com.dotori.v2.domain.member.enums.Role
 import java.time.LocalDate
 
 data class BoardAlarmResDto(
-    val id: Long,
-    val title: String,
-    val writerRole: List<Role>,
-    val lastBoardWriteDate: LocalDate
-) {
-    constructor(board: Board) : this(id = board.id, title = board.title, writerRole = board.member.roles, lastBoardWriteDate = board.createdDate!!.toLocalDate())
-}
+    val content: List<BoardDto>
+)
