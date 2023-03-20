acc20967a7eb40d280338403de0ef2dad830a4cb
Huuuunee <s21024@gsm.hs.kr>
2023-03-20 19:51:05 +0900

    :recycle: 트랜잭션 추가

--- src/main/kotlin/com/dotori/v2/domain/rule/service/impl/DeleteRuleServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/rule/service/impl/DeleteRuleServiceImpl.kt
@@ -5,8 +5,10 @@ import com.dotori.v2.domain.rule.exception.NotFoundRuleException
 import com.dotori.v2.domain.rule.service.DeleteRuleService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 
 @Service
+@Transactional(rollbackFor = [Exception::class])
 class DeleteRuleServiceImpl(
     private val ruleRepository: RuleRepository
 ) : DeleteRuleService {
