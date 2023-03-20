8c08e444ed6017d5ee246b0c132729a3c991b54b
Huuuunee <s21024@gsm.hs.kr>
2023-03-20 17:00:08 +0900

    :sparkles: 규정위반 삭제 기능 구현

---
+++ src/main/kotlin/com/dotori/v2/domain/rule/service/impl/DeleteRuleServiceImpl.kt
@@ -0,0 +1,17 @@
+package com.dotori.v2.domain.rule.service.impl
+
+import com.dotori.v2.domain.rule.domain.repository.RuleRepository
+import com.dotori.v2.domain.rule.exception.NotFoundRuleException
+import com.dotori.v2.domain.rule.service.DeleteRuleService
+import org.springframework.data.repository.findByIdOrNull
+import org.springframework.stereotype.Service
+
+@Service
+class DeleteRuleServiceImpl(
+    private val ruleRepository: RuleRepository
+) : DeleteRuleService {
+    override fun execute(ruleId: Long) =
+        ruleRepository.findByIdOrNull(ruleId)
+            .let { if (it == null) throw NotFoundRuleException() else ruleRepository.delete(it) }
+
+}
