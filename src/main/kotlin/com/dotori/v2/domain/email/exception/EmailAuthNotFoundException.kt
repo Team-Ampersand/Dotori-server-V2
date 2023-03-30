66d97d63ee9f33285356b3c4840fab6851cd6d5d
jeayoung <k01066624566@gmail.com>
2023-03-30 10:47:58 +0900

    이메일 인증을 못 찾을때 예외 변경

--- src/main/kotlin/com/dotori/v2/domain/email/exception/EmailAuthNotFoundException.kt
+++ src/main/kotlin/com/dotori/v2/domain/email/exception/EmailAuthNotFoundException.kt
@@ -3,5 +3,5 @@ package com.dotori.v2.domain.email.exception
 import com.dotori.v2.global.error.ErrorCode
 import com.dotori.v2.global.error.exception.BasicException
 
-class EmailSendFailException : BasicException(ErrorCode.EMAIL_SEND_FAIL){
+class EmailAuthNotFoundException : BasicException(ErrorCode.MAIL_AUTH_NOT_FOUND){
 }
