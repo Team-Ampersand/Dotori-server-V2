9200b50a448786a1d082458df781196e837d3172
jeayoung <k01066624566@gmail.com>
2023-03-30 20:40:28 +0900

    자습 안마정보보낼때 인원수가 다 차있으면 CANT반환

--- src/main/kotlin/com/dotori/v2/domain/self_study/service/impl/GetSelfStudyInfoServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/self_study/service/impl/GetSelfStudyInfoServiceImpl.kt
@@ -2,9 +2,9 @@ package com.dotori.v2.domain.self_study.service.impl
 
 import com.dotori.v2.domain.member.enums.SelfStudyStatus
 import com.dotori.v2.domain.self_study.domain.repository.SelfStudyCountRepository
+import com.dotori.v2.domain.self_study.excetpion.SelfStudyOverException
 import com.dotori.v2.domain.self_study.presentation.dto.res.SelfStudyInfoResDto
 import com.dotori.v2.domain.self_study.service.GetSelfStudyInfoService
-import com.dotori.v2.domain.self_study.util.SelfStudyCheckUtil
 import com.dotori.v2.domain.self_study.util.ValidDayOfWeekAndHourUtil
 import com.dotori.v2.global.error.exception.BasicException
 import com.dotori.v2.global.util.UserUtil
@@ -23,6 +23,8 @@ class GetSelfStudyInfoServiceImpl(
         val member = userUtil.fetchCurrentUser()
         try {
             validDayOfWeekAndHourUtil.validateApply()
+            if (selfStudyCount.count >= selfStudyCount.limit)
+                throw SelfStudyOverException()
         } catch (ex: BasicException){
             return SelfStudyInfoResDto(
                 count = selfStudyCount.count,
