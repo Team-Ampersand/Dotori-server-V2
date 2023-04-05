ddd5f333443321cfcda4dea05e46c9e7599a991f
Huuuunee <s21024@gsm.hs.kr>
2023-04-05 20:58:17 +0900

    :sparkles: 자기자신의 음악신청 취소하는 API생성

--- src/main/kotlin/com/dotori/v2/domain/music/presentation/member/MemberMusicController.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/presentation/member/MemberMusicController.kt
@@ -3,6 +3,7 @@ package com.dotori.v2.domain.music.presentation.member
 import com.dotori.v2.domain.music.presentation.data.req.ApplyMusicReqDto
 import com.dotori.v2.domain.music.presentation.data.res.MusicListResDto
 import com.dotori.v2.domain.music.service.ApplyMusicService
+import com.dotori.v2.domain.music.service.DeleteMusicService
 import com.dotori.v2.domain.music.service.FindMusicsService
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
@@ -15,7 +16,8 @@ import java.time.LocalDateTime
 @RequestMapping("/v2/member/music")
 class MemberMusicController(
     private val applyMusicService: ApplyMusicService,
-    private val findMusicsService: FindMusicsService
+    private val findMusicsService: FindMusicsService,
+    private val deleteMusicService: DeleteMusicService
 ) {
     @PostMapping
     fun applyMusic(@RequestBody applyMusicReqDto: ApplyMusicReqDto): ResponseEntity<Void> =
@@ -30,4 +32,9 @@ class MemberMusicController(
         ) @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) date: LocalDate
     ): ResponseEntity<MusicListResDto> =
         ResponseEntity.status(HttpStatus.OK).body(findMusicsService.execute(date))
+
+    @DeleteMapping("/{music_id}")
+    fun deleteMusic(@PathVariable music_id: Long): ResponseEntity<Void> =
+        deleteMusicService.execute(music_id)
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 }
