e99c199732cb125f2323695d6631b1878cfcef83
전승원 <78971821+louis7308@users.noreply.github.com>
2023-05-04 13:51:13 +0900

    :ambulance: :: test domain을 companion object 로 변경

--- src/test/kotlin/com/dotori/v2/domain/music/util/MusicUtil.kt
+++ src/test/kotlin/com/dotori/v2/domain/music/util/MusicUtil.kt
@@ -4,12 +4,13 @@ import com.dotori.v2.domain.member.domain.entity.Member
 import com.dotori.v2.domain.member.util.MemberUtil
 import com.dotori.v2.domain.music.domain.entity.Music
 
-class MusicUtil(
-    private val memberUtil: MemberUtil
-) {
-    fun createMusic(
-        id: Long = 0,
-        url: String = "https://www.test.url",
-        member: Member = memberUtil.createMember()
-    ) = Music(id, url, member)
+class MusicUtil {
+    companion object {
+        fun createMusic(
+            id: Long = 0,
+            url: String = "https://www.test.url",
+            member: Member = MemberUtil.createMember()
+        ) = Music(id, url, member)
+    }
+
 }
