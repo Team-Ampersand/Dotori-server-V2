2c92991bf960853d7c794c176291f0707884b438
aesperer <piyrw9754@gmail.com>
2023-07-29 21:53:39 +0900

    update :: board alarm service, dto

--- src/main/kotlin/com/dotori/v2/domain/board/presentation/data/dto/BoardDto.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/presentation/data/dto/BoardDto.kt
@@ -1,13 +1,17 @@
 package com.dotori.v2.domain.board.presentation.data.dto
 
+import com.dotori.v2.domain.board.domain.entity.Board
 import com.dotori.v2.domain.member.enums.Role
 import java.time.LocalDate
-import java.time.LocalDateTime
 
 data class BoardDto(
     val id: Long,
     val title: String,
     val roles: List<Role>,
     val boardImages: List<BoardImageDto>,
-    val createdDate: LocalDateTime
-)
+    val createdDate: LocalDate
+) {
+    companion object {
+        fun of(board: Board): BoardDto =
+    }
+}
