78be8d67d39ba122b2b5ac2ae2392ac97bd8ebb8
KimGyeongsuuu <sk62628504@gmail.com>
2023-10-18 14:01:53 +0900

    update :: profileImage 필드 추가

--- src/main/kotlin/com/dotori/v2/domain/main_page/presentation/dto/res/PersonalInfoResDto.kt
+++ src/main/kotlin/com/dotori/v2/domain/main_page/presentation/dto/res/PersonalInfoResDto.kt
@@ -7,7 +7,8 @@ data class PersonalInfoResDto(
     val id: Long,
     val stuNum: String,
     val name: String,
-    val gender: Gender
+    val gender: Gender,
+    val profileImage: String?
 ) {
-    constructor(member: Member) : this(id = member.id, stuNum = member.stuNum, name = member.memberName, gender = member.gender)
+    constructor(member: Member) : this(id = member.id, stuNum = member.stuNum, name = member.memberName, gender = member.gender, profileImage = member.profileImage)
 }
