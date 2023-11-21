ab2b64b1d424aa39ad2fa4c39c96ca2a98e72587
uuuuuuuk <s22034@gsm.hs.kr>
2023-11-21 19:20:51 +0900

    :memo: jwt properties 작성

---
+++ src/main/kotlin/com/dotori/v2/global/security/jwt/properties/JwtProperties.kt
@@ -0,0 +1,22 @@
+package com.dotori.v2.global.security.jwt.properties
+
+import io.jsonwebtoken.security.Keys
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+import java.security.Key
+
+@ConstructorBinding
+@ConfigurationProperties(prefix = "security.jwt.token")
+class JwtProperties(
+    accessSecret: String,
+    refreshSecret: String
+) {
+
+    val accessSecret: Key
+    val refreshSecret: Key
+
+    init {
+        this.accessSecret = Keys.hmacShaKeyFor(accessSecret.toByteArray())
+        this.refreshSecret = Keys.hmacShaKeyFor(refreshSecret.toByteArray())
+    }
+}
