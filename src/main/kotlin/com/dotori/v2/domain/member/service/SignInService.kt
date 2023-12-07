e797b4c5bc0b8fd0df4d4faa1075a69f5749e1fc
huuuunee <s21024@gsm.hs.kr>
2023-12-07 11:52:51 +0900

    :fire: 옮기면서 필요없는 파일 삭제하였습니다.

--- src/main/kotlin/com/dotori/v2/domain/member/service/SignInService.kt
+++
@@ -1,8 +0,0 @@
-package com.dotori.v2.domain.member.service
-
-import com.dotori.v2.domain.member.presentation.data.dto.SignInDto
-import com.dotori.v2.domain.member.presentation.data.res.SignInResDto
-
-interface SignInService {
-    fun execute(signInDto: SignInDto): SignInResDto
-}
