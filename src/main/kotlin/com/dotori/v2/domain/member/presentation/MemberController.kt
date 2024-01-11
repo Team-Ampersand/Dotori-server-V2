46655db6fbf64ed80926b489215ac3c9c9ab983c
uuuuuuuk <s22034@gsm.hs.kr>
2024-01-11 17:31:45 +0900

    :sparkles: MemberController 에 복구된 api 추가

--- src/main/kotlin/com/dotori/v2/domain/member/presentation/MemberController.kt
+++ src/main/kotlin/com/dotori/v2/domain/member/presentation/MemberController.kt
@@ -1,19 +1,30 @@
 package com.dotori.v2.domain.member.presentation
 
-import com.dotori.v2.domain.member.service.DeleteProfileImageService
-import com.dotori.v2.domain.member.service.UpdateProfileImageService
-import com.dotori.v2.domain.member.service.UploadProfileImageService
+import com.dotori.v2.domain.member.presentation.data.req.NewPasswordReqDto
+import com.dotori.v2.domain.member.service.*
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/v2/members")
 class MemberController(
+    private val withdrawalService: WithdrawalService,
+    private val changeAuthPasswordService: ChangeAuthPasswordService,
     private val uploadProfileImageService: UploadProfileImageService,
     private val updateProfileImageService: UpdateProfileImageService,
     private val deleteProfileImageService: DeleteProfileImageService
 ) {
+    @DeleteMapping("/withdrawal")
+    fun withdrawal(): ResponseEntity<Void> =
+        withdrawalService.execute()
+            .run { ResponseEntity.ok().build() }
+
+    @PatchMapping("/password")
+    fun changePassword(@Valid @RequestBody newPasswordReqDto: NewPasswordReqDto): ResponseEntity<Void> =
+        changeAuthPasswordService.execute(newPasswordReqDto)
+            .run { ResponseEntity.ok().build() }
 
     @PostMapping("/profileImage")
     fun uploadProfileImage(@RequestParam(value = "image") multipartFiles: MultipartFile?): ResponseEntity<Void> =
