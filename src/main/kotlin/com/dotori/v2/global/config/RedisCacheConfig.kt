5902004c6e43fbd2ea4451b886b576a5eceef211
KimGyeongsuuu <sk62628504@gmail.com>
2024-05-30 14:15:14 +0900

    add :: redis cache config

---
+++ src/main/kotlin/com/dotori/v2/global/config/RedisCacheConfig.kt
@@ -0,0 +1,52 @@
+package com.dotori.v2.global.config
+
+import com.dotori.v2.domain.board.presentation.data.res.ListBoardResDto
+import com.fasterxml.jackson.annotation.JsonAutoDetect
+import com.fasterxml.jackson.annotation.PropertyAccessor
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.cache.CacheManager
+import org.springframework.cache.annotation.EnableCaching
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.redis.cache.RedisCacheConfiguration
+import org.springframework.data.redis.cache.RedisCacheManager
+import org.springframework.data.redis.connection.RedisConnectionFactory
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer
+import org.springframework.data.redis.serializer.RedisSerializationContext
+import org.springframework.data.redis.serializer.StringRedisSerializer
+import java.time.Duration
+
+@Configuration
+@EnableCaching
+class RedisCacheConfig {
+
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        return jacksonObjectMapper().apply {
+            registerModule(JavaTimeModule())
+            registerModule(KotlinModule())
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
+
+    @Bean
+    fun contentCacheManager(connectionFactory: RedisConnectionFactory, objectMapper: ObjectMapper): CacheManager {
+        val serializer = Jackson2JsonRedisSerializer(ListBoardResDto::class.java).apply {
+            setObjectMapper(objectMapper)
+        }
+
+        val redisCacheConfiguration = RedisCacheConfiguration.defaultCacheConfig()
+            .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(StringRedisSerializer()))
+            .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+            .entryTtl(Duration.ofMinutes(10L))
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(connectionFactory)
+            .cacheDefaults(redisCacheConfiguration)
+            .build()
+    }
+}
