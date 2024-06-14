029b154f56402a0f05bac8528e91205bfae29ff1
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-14 12:07:43 +0900

    add :: cacheable annotation

--- src/main/kotlin/com/dotori/v2/domain/board/service/impl/GetBoardsServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/service/impl/GetBoardsServiceImpl.kt
@@ -4,16 +4,23 @@ import com.dotori.v2.domain.board.domain.repository.BoardRepository
 import com.dotori.v2.domain.board.presentation.data.res.BoardResDto
 import com.dotori.v2.domain.board.presentation.data.res.ListBoardResDto
 import com.dotori.v2.domain.board.service.GetBoardsService
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
 @Service
 @Transactional(readOnly = true, rollbackFor = [Exception::class])
 class GetBoardsServiceImpl(
+
     private val boardRepository: BoardRepository,
 ) : GetBoardsService {
 
-    override fun execute(): ListBoardResDto = ListBoardResDto(
+    @Cacheable(
+        value = ["BoardList"],
+        cacheManager = "contentCacheManager",
+    )
+    override fun execute(): ListBoardResDto =
+        ListBoardResDto(
         boardList = boardRepository.findAllByOrderByCreatedDateDesc()
             .map { BoardResDto.of(it) }
     )
