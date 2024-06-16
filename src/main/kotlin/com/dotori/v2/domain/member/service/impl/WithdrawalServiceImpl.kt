012f79d5062eb8387cb3ba0d450a9c4cbb2e60c5
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-16 21:22:44 +0900

    add :: 회원탈퇴 API에 CacheEvict annotation을 통한 Write Around 쓰기 캐시 전략 적용

--- src/main/kotlin/com/dotori/v2/domain/member/service/impl/WithdrawalServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/member/service/impl/WithdrawalServiceImpl.kt
@@ -4,6 +4,7 @@ import com.dotori.v2.domain.member.domain.repository.MemberRepository
 import com.dotori.v2.domain.member.service.WithdrawalService
 import com.dotori.v2.global.thirdparty.aws.s3.S3Service
 import com.dotori.v2.global.util.UserUtil
+import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,6 +15,7 @@ class WithdrawalServiceImpl(
     private val s3Service: S3Service,
     private val userUtil: UserUtil,
 ) : WithdrawalService {
+    @CacheEvict(cacheNames = ["memberList"], key = "'memberList'", cacheManager = "contentCacheManager")
     override fun execute() {
         val currentUser = userUtil.fetchCurrentUser()
         currentUser.profileImage?.let { s3Service.deleteFile(it) }
