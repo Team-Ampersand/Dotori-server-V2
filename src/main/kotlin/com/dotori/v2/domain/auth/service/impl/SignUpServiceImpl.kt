36d3c33ae71541f493e24f0959a354b5e75108d4
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-17 15:14:10 +0900

    update :: 회원가입 API를 redisTemplate 함수를 통해 직접 구현하는 방식으로 변경

--- src/main/kotlin/com/dotori/v2/domain/auth/service/impl/SignUpServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/auth/service/impl/SignUpServiceImpl.kt
@@ -5,9 +5,12 @@ import com.dotori.v2.domain.auth.service.SignUpService
 import com.dotori.v2.domain.email.domain.repository.EmailCertificateRepository
 import com.dotori.v2.domain.email.exception.EmailAuthNotFoundException
 import com.dotori.v2.domain.email.exception.EmailNotBeenException
+import com.dotori.v2.domain.member.domain.entity.Member
 import com.dotori.v2.domain.member.domain.repository.MemberRepository
+import com.dotori.v2.domain.member.enums.Role
 import com.dotori.v2.domain.member.exception.MemberAlreadyException
-import org.springframework.cache.annotation.CacheEvict
+import com.dotori.v2.domain.student.presentation.data.res.FindAllStudentResDto
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -18,8 +21,8 @@ class SignUpServiceImpl(
     private val emailCertificateRepository: EmailCertificateRepository,
     private val memberRepository: MemberRepository,
     private val passwordEncoder: PasswordEncoder,
+    private val redisCacheService: RedisCacheService
 ): SignUpService {
-    @CacheEvict(cacheNames = ["memberList"], key = "'memberList'", cacheManager = "contentCacheManager")
     override fun execute(signUpReqDto: SignUpReqDto) {
         val emailCertificate = emailCertificateRepository.findByEmail(signUpReqDto.email)
             ?: throw EmailAuthNotFoundException()
@@ -36,5 +39,29 @@ class SignUpServiceImpl(
         val member = signUpReqDto.toEntity(encodedPassword)
 
         memberRepository.save(member)
+
+        updateCache(member)
+    }
+
+    private fun updateCache(member: Member) {
+        val cacheKey = "memberList"
+        val cachedData = redisCacheService.getFromCache(cacheKey) as? List<FindAllStudentResDto>
+
+        if (cachedData != null) {
+            val updatedList = cachedData.toMutableList().apply {
+                add(
+                    FindAllStudentResDto(
+                        id = member.id,
+                        gender = member.gender,
+                        memberName = member.memberName,
+                        stuNum = member.stuNum,
+                        role = Role.ROLE_MEMBER,
+                        selfStudyStatus = member.selfStudyStatus,
+                        profileImage = null
+                    )
+                )
+            }
+            redisCacheService.putToCache(cacheKey, updatedList)
+        }
     }
 }
