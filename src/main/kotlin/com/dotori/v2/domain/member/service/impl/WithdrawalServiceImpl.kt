ee7520f82a922ef9c7c4fe16bf4192b347318e74
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-17 15:14:56 +0900

    update :: 회원탈퇴 API를 redisTemplate 함수를 통해 직접 구현하는 방식으로 변경

--- src/main/kotlin/com/dotori/v2/domain/member/service/impl/WithdrawalServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/member/service/impl/WithdrawalServiceImpl.kt
@@ -2,9 +2,10 @@ package com.dotori.v2.domain.member.service.impl
 
 import com.dotori.v2.domain.member.domain.repository.MemberRepository
 import com.dotori.v2.domain.member.service.WithdrawalService
+import com.dotori.v2.domain.student.presentation.data.res.FindAllStudentResDto
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import com.dotori.v2.global.thirdparty.aws.s3.S3Service
 import com.dotori.v2.global.util.UserUtil
-import org.springframework.cache.annotation.CacheEvict
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 
@@ -14,11 +15,23 @@ class WithdrawalServiceImpl(
     private val memberRepository: MemberRepository,
     private val s3Service: S3Service,
     private val userUtil: UserUtil,
+    private val redisCacheService: RedisCacheService
 ) : WithdrawalService {
-    @CacheEvict(cacheNames = ["memberList"], key = "'memberList'", cacheManager = "contentCacheManager")
     override fun execute() {
         val currentUser = userUtil.fetchCurrentUser()
         currentUser.profileImage?.let { s3Service.deleteFile(it) }
         memberRepository.delete(currentUser)
+
+        evictUserFromCache(currentUser.id)
+    }
+
+    private fun evictUserFromCache(userId: Long) {
+        val cacheKey = "memberList"
+        val cachedData = redisCacheService.getFromCache(cacheKey) as? List<FindAllStudentResDto>
+
+        if (cachedData != null) {
+            val updatedList = cachedData.filterNot { it.id == userId }
+            redisCacheService.putToCache(cacheKey, updatedList)
+        }
     }
 }
