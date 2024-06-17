a82ed5103b454d6eb8cef9c36454d6c530f26721
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-17 15:16:34 +0900

    update :: 학생 정보 수정 API를 redisTemplate 함수를 통해 직접 구현하는 방식으로 변경

--- src/main/kotlin/com/dotori/v2/domain/student/service/impl/ModifyStudentInfoServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/student/service/impl/ModifyStudentInfoServiceImpl.kt
@@ -4,8 +4,9 @@ import com.dotori.v2.domain.member.domain.entity.Member
 import com.dotori.v2.domain.member.domain.repository.MemberRepository
 import com.dotori.v2.domain.member.exception.MemberNotFoundException
 import com.dotori.v2.domain.student.presentation.data.req.ModifyStudentInfoRequest
+import com.dotori.v2.domain.student.presentation.data.res.FindAllStudentResDto
 import com.dotori.v2.domain.student.service.ModifyStudentInfoService
-import org.springframework.cache.annotation.CacheEvict
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -14,12 +15,17 @@ import java.util.*
 @Service
 @Transactional(rollbackFor = [Exception::class])
 class ModifyStudentInfoServiceImpl(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val redisCacheService: RedisCacheService,
 ) : ModifyStudentInfoService {
-    @CacheEvict(cacheNames = ["memberList"], key = "'memberList'", cacheManager = "contentCacheManager")
+
     override fun execute(modifyStudentInfoRequest: ModifyStudentInfoRequest) {
-        val member = memberRepository.findByIdOrNull(modifyStudentInfoRequest.memberId) ?: throw MemberNotFoundException()
+        val member = memberRepository.findByIdOrNull(modifyStudentInfoRequest.memberId)
+            ?: throw MemberNotFoundException()
+
         updateMemberInfo(member, modifyStudentInfoRequest)
+
+        updateCache(modifyStudentInfoRequest)
     }
 
     private fun updateMemberInfo(member: Member, modifyStudentInfoRequest: ModifyStudentInfoRequest) {
@@ -35,5 +41,29 @@ class ModifyStudentInfoServiceImpl(
             profileImage = member.profileImage
         )
         memberRepository.save(newMember)
+    }
+
+    private fun updateCache(modifyStudentInfoRequest: ModifyStudentInfoRequest) {
+        val cacheKey = "memberList"
+        val cachedData = redisCacheService.getFromCache(cacheKey) as? List<FindAllStudentResDto>?
+
+        if (cachedData != null) {
+            val updatedList = cachedData.map {
+                if (it.id == modifyStudentInfoRequest.memberId) {
+                    FindAllStudentResDto(
+                        id = it.id,
+                        gender = modifyStudentInfoRequest.gender,
+                        memberName = modifyStudentInfoRequest.memberName,
+                        stuNum = modifyStudentInfoRequest.stuNum,
+                        role = modifyStudentInfoRequest.role,
+                        selfStudyStatus = it.selfStudyStatus,
+                        profileImage = it.profileImage
+                    )
+                } else {
+                    it
+                }
+            }
+            redisCacheService.putToCache(cacheKey, updatedList)
+        }
     }
 }
