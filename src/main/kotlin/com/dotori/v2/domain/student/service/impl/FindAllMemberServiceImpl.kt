fd174f167828fda51c7e4b52cf86e8732bbeea33
uuuuuuuk <s22034@gsm.hs.kr>
2024-06-17 15:13:42 +0900

    update :: 학생정보 조회 어드민 API를 redisTemplate 함수를 통해 직접 구현하는 방식으로 변경

--- src/main/kotlin/com/dotori/v2/domain/student/service/impl/FindAllMemberServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/student/service/impl/FindAllMemberServiceImpl.kt
@@ -3,7 +3,7 @@ package com.dotori.v2.domain.student.service.impl
 import com.dotori.v2.domain.member.domain.repository.MemberRepository
 import com.dotori.v2.domain.student.presentation.data.res.FindAllStudentResDto
 import com.dotori.v2.domain.student.service.FindAllMemberService
-import org.springframework.cache.annotation.Cacheable
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import org.springframework.data.domain.Sort
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -12,21 +12,31 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(readOnly = true, rollbackFor = [Exception::class])
 class FindAllMemberServiceImpl(
-    private val memberRepository: MemberRepository
+    private val memberRepository: MemberRepository,
+    private val redisCacheService: RedisCacheService
 ) : FindAllMemberService {
-    @Cacheable(cacheNames = ["memberList"], key = "'memberList'", cacheManager = "contentCacheManager")
-    override fun execute(): List<FindAllStudentResDto> =
-        memberRepository.findAll(Sort.by(Sort.Direction.ASC, "stuNum"))
-            .map {
-                FindAllStudentResDto(
-                    id = it.id,
-                    gender = it.gender,
-                    memberName = it.memberName,
-                    stuNum = it.stuNum,
-                    role = it.roles[0],
-                    selfStudyStatus = it.selfStudyStatus,
-                    profileImage = it.profileImage
-                )
-            }
+    override fun execute(): List<FindAllStudentResDto> {
+        val cacheKey = "memberList"
 
+        val cachedData = redisCacheService.getFromCache(cacheKey)
+        if (cachedData != null) {
+            return cachedData as List<FindAllStudentResDto>
+        }
+
+        val members = memberRepository.findAll(Sort.by(Sort.Direction.ASC, "stuNum")).map {
+            FindAllStudentResDto(
+                id = it.id,
+                gender = it.gender,
+                memberName = it.memberName,
+                stuNum = it.stuNum,
+                role = it.roles[0],
+                selfStudyStatus = it.selfStudyStatus,
+                profileImage = it.profileImage
+            )
+        }
+
+        redisCacheService.putToCache(cacheKey, members)
+
+        return members
+    }
 }
