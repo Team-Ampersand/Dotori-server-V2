812ca76ca65307afb27da6bec924ceefac7d839b
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-21 16:11:23 +0900

    feat :: modify board service cache

--- src/main/kotlin/com/dotori/v2/domain/board/service/impl/ModifyBoardServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/board/service/impl/ModifyBoardServiceImpl.kt
@@ -5,7 +5,10 @@ import com.dotori.v2.domain.board.domain.repository.BoardRepository
 import com.dotori.v2.domain.board.exception.BoardNotExistsException
 import com.dotori.v2.domain.board.presentation.data.dto.ModifyBoardDto
 import com.dotori.v2.domain.board.presentation.data.req.ModifyBoardReqDto
+import com.dotori.v2.domain.board.presentation.data.res.BoardResDto
+import com.dotori.v2.domain.board.presentation.data.res.ListBoardResDto
 import com.dotori.v2.domain.board.service.ModifyBoardService
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -13,7 +16,8 @@ import org.springframework.transaction.annotation.Transactional
 @Service
 @Transactional(rollbackFor = [Exception::class])
 class ModifyBoardServiceImpl(
-    private val boardRepository: BoardRepository
+    private val boardRepository: BoardRepository,
+    private val redisCacheService: RedisCacheService
 ) : ModifyBoardService {
 
     override fun execute(modifyBoardReqDto: ModifyBoardReqDto, boardId: Long) {
@@ -23,6 +27,26 @@ class ModifyBoardServiceImpl(
         toDto(modifyBoardReqDto)
             .let { boardInfo.updateBoard(title = it.title, content = it.content) }
 
+        updateBoardCache(boardInfo)
+
+    }
+    private fun updateBoardCache(board: Board) {
+        val cacheKey = "boardList"
+        val cachedData = redisCacheService.getFromCache(cacheKey) as? ListBoardResDto
+
+        if (cachedData != null) {
+            val updatedList = cachedData.boardList.map {
+                if (it.id == board.id) {
+                  BoardResDto.of(board)
+                } else {
+                    it
+                }
+            }.toMutableList()
+
+            updatedList.sortByDescending { it.id }
+
+            redisCacheService.putToCache(cacheKey, ListBoardResDto(updatedList))
+        }
     }
 
     private fun toDto(modifyBoardReqDto: ModifyBoardReqDto): ModifyBoardDto =
