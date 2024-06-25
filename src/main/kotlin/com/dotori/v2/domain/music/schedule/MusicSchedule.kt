bf151dd595aaca707d0dc09f469ce21f25605688
aesperer <piyrw9754@gmail.com>
2024-06-25 14:20:30 +0900

    add :: cache data init schedule

--- src/main/kotlin/com/dotori/v2/domain/music/schedule/MusicSchedule.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/schedule/MusicSchedule.kt
@@ -1,7 +1,9 @@
 package com.dotori.v2.domain.music.schedule
 
 import com.dotori.v2.domain.music.domain.repository.MusicRepository
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import org.slf4j.LoggerFactory
+import org.springframework.data.redis.core.RedisTemplate
 import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
@@ -10,13 +12,18 @@ import java.time.LocalDate
 @Component
 @Transactional
 class MusicSchedule(
-    private val musicRepository: MusicRepository
+    private val musicRepository: MusicRepository,
+    private val redisTemplate: RedisTemplate<String, Any>
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
 
     @Scheduled(cron = "0 0 0 ? * MON-FRI")
     fun weekdayMusicStatusReset() {
         musicRepository.updateMusicStatusMemberByMember()
+        val keys = redisTemplate.keys("musicList:*")
+        if(keys.isNotEmpty()) {
+            redisTemplate.delete(keys)
+        }
         log.info("Student Music Status Updated At {}", LocalDate.now())
     }
 }
