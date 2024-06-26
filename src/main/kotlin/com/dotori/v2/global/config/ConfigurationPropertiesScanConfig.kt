e205fa5d034c6feebba4b18cf9e709653d3c2603
KimGyeongsuuu <sk62628504@gmail.com>
2024-06-26 17:34:08 +0900

    delete :: CacheKeyProperties properties scan

--- src/main/kotlin/com/dotori/v2/global/config/ConfigurationPropertiesScanConfig.kt
+++ src/main/kotlin/com/dotori/v2/global/config/ConfigurationPropertiesScanConfig.kt
@@ -1,7 +1,6 @@
 package com.dotori.v2.global.config
 
 import com.dotori.v2.global.config.gauth.properties.GAuthProperties
-import com.dotori.v2.global.config.redis.properties.CacheKeyProperties
 import com.dotori.v2.global.security.jwt.properties.JwtProperties
 import com.dotori.v2.global.security.jwt.properties.JwtTimeProperties
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
@@ -12,8 +11,7 @@ import org.springframework.context.annotation.Configuration
     basePackageClasses = [
         GAuthProperties::class,
         JwtProperties::class,
-        JwtTimeProperties::class,
-        CacheKeyProperties::class
+        JwtTimeProperties::class
     ]
 )
 class ConfigurationPropertiesScanConfig
