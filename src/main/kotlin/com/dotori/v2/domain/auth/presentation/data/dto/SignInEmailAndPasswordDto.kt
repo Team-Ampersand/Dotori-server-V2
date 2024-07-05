cb3000e3cfb94cee1437790e8ee12eae8cd3d64d
aesperer <piyrw9754@gmail.com>
2024-07-05 11:30:03 +0900

    delete :: deprecated delete

--- src/main/kotlin/com/dotori/v2/domain/auth/presentation/data/dto/SignInEmailAndPasswordDto.kt
+++ src/main/kotlin/com/dotori/v2/domain/auth/presentation/data/dto/SignInEmailAndPasswordDto.kt
@@ -1,6 +1,5 @@
 package com.dotori.v2.domain.auth.presentation.data.dto
 
-@Deprecated("GAuth 로그인  변경 때문입니다.")
 data class SignInEmailAndPasswordDto(
     val email: String,
     val password: String
