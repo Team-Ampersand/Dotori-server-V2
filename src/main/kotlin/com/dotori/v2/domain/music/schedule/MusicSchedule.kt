c06784ef7b3fcdf6e0663ff8a7d463a8b33a56db
aesperer <piyrw9754@gmail.com>
2024-07-05 10:15:25 +0900

    chore :: 2달전 음악 삭제 로직, 캐시 초기화 캐시 서비스 로직 공통으로 분리

--- src/main/kotlin/com/dotori/v2/domain/music/schedule/MusicSchedule.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/schedule/MusicSchedule.kt
@@ -8,22 +8,30 @@ import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
 import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
 
 @Component
 @Transactional
 class MusicSchedule(
     private val musicRepository: MusicRepository,
-    private val redisTemplate: RedisTemplate<String, Any>
+    private val redisCacheService: RedisCacheService
 ) {
     private val log = LoggerFactory.getLogger(this.javaClass.simpleName)
 
     @Scheduled(cron = "0 0 0 ? * MON-FRI")
     fun weekdayMusicStatusReset() {
         musicRepository.updateMusicStatusMemberByMember()
-        val keys = redisTemplate.keys("musicList:*")
-        if(keys.isNotEmpty()) {
-            redisTemplate.delete(keys)
-        }
-        log.info("Student Music Status Updated At {}", LocalDate.now())
+        redisCacheService.initCache("musicList:*")
+        log.info("init member music status")
     }
+
+    @Scheduled(cron = "0 0 0 1 *")
+    fun initMusic() {
+        val localDateTime = LocalDateTime.now().minus(2,ChronoUnit.MONTHS)
+        musicRepository.deleteAllByCreatedDateBefore(localDateTime)
+        redisCacheService.initCache("musicList:*")
+        log.info("delete music data schedule")
+    }
+
 }
