544594dbec8da3e2b9196faf30c54206b571ac74
KimGyeongsuuu <sk62628504@gmail.com>
2024-07-20 23:19:52 +0900

    update :: error filter

--- src/main/kotlin/com/dotori/v2/global/error/ErrorFilter.kt
+++ src/main/kotlin/com/dotori/v2/global/error/ErrorFilter.kt
@@ -2,10 +2,13 @@ package com.dotori.v2.global.error
 
 import com.dotori.v2.global.error.exception.BasicException
 import com.fasterxml.jackson.databind.ObjectMapper
+import io.jsonwebtoken.ExpiredJwtException
+import io.jsonwebtoken.JwtException
 import javax.servlet.FilterChain
 import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
@@ -24,6 +27,18 @@ class ErrorFilter : OncePerRequestFilter() {
             filterChain.doFilter(request, response)
         }.onFailure { e ->
             when (e) {
+                is ExpiredJwtException -> {
+                    log.error("Expired Jwt Exception Occurred - Message = {}, Status = {}", e.message, HttpStatus.UNAUTHORIZED)
+                    sendError(response, ErrorResponse(ErrorCode.TOKEN_EXPIRED))
+                }
+                is JwtException -> {
+                    log.error("Invalid Jwt Exception Occurred - Message = {}, Status = {}", e.message, HttpStatus.UNAUTHORIZED)
+                    sendError(response, ErrorResponse(ErrorCode.TOKEN_INVALID))
+                }
+                is IllegalArgumentException -> {
+                    log.error("Illegal Argument Exception Occurred - Message = {}, Status = {}", e.message, HttpStatus.BAD_REQUEST)
+                    sendError(response, ErrorResponse(ErrorCode.BAD_REQUEST))
+                }
                 is BasicException -> {
                     log.error("Basic Exception Occurred - Message = {}, Status = {}", e.message, e.errorCode.error)
                     sendError(response, ErrorResponse(e.errorCode))
