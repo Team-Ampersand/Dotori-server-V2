79b68d7b1a90c91d66b51ac294033dc372a7a5b5
aesperer <piyrw9754@gmail.com>
2024-08-08 21:10:03 +0900

    delete :: properties self study

--- src/main/kotlin/com/dotori/v2/global/config/ConfigurationPropertiesScanConfig.kt
+++ src/main/kotlin/com/dotori/v2/global/config/ConfigurationPropertiesScanConfig.kt
@@ -1,6 +1,5 @@
 package com.dotori.v2.global.config
 
-import com.dotori.v2.domain.selfstudy.properties.SelfStudyProperties
 import com.dotori.v2.global.config.gauth.properties.GAuthProperties
 import com.dotori.v2.global.security.jwt.properties.JwtProperties
 import com.dotori.v2.global.security.jwt.properties.JwtTimeProperties
@@ -12,7 +11,7 @@ import org.springframework.context.annotation.Configuration
     basePackageClasses = [
         GAuthProperties::class,
         JwtProperties::class,
-        JwtTimeProperties::class,
+        JwtTimeProperties::class
     ]
 )
 class ConfigurationPropertiesScanConfig
