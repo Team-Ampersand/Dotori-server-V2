5bda02b935e8bc0e474f19c7fe04f71a8890c022
ta2ye0n <tyeon42051@naver.com>
2024-09-11 17:02:25 +0900

    add :: 좋아요, 좋아요 취소 controller에 추가

--- src/main/kotlin/com/dotori/v2/domain/music/presentation/admin/AdminMusicController.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/presentation/admin/AdminMusicController.kt
@@ -1,8 +1,10 @@
 package com.dotori.v2.domain.music.presentation.admin
 
 import com.dotori.v2.domain.music.presentation.data.res.MusicListResDto
+import com.dotori.v2.domain.music.service.DeleteMusicLikeService
 import com.dotori.v2.domain.music.service.DeleteMusicService
 import com.dotori.v2.domain.music.service.FindMusicsService
+import com.dotori.v2.domain.music.service.PostMusicLikeService
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -13,7 +15,9 @@ import java.time.LocalDate
 @RequestMapping("/v2/admin/music")
 class AdminMusicController(
     private val findMusicsService: FindMusicsService,
-    private val deleteMusicService: DeleteMusicService
+    private val deleteMusicService: DeleteMusicService,
+    private val postMusicLikeService: PostMusicLikeService,
+    private val deleteMusicLikeService: DeleteMusicLikeService,
 ) {
     @GetMapping
     fun findMusics(
@@ -27,6 +31,16 @@ class AdminMusicController(
     @DeleteMapping("/{music_id}")
     fun deleteMusic(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
         deleteMusicService.execute(musicId)
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @PostMapping("/{music_id}/like")
+    fun postMusicLike(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
+        postMusicLikeService.execute(musicId)
+            .run { ResponseEntity.status(HttpStatus.OK).build() }
+
+    @DeleteMapping("/{music_id}/like")
+    fun deleteMusicLike(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
+        deleteMusicLikeService.execute(musicId)
             .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 
 }
--- src/main/kotlin/com/dotori/v2/domain/music/presentation/councillor/CouncillorMusicController.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/presentation/councillor/CouncillorMusicController.kt
@@ -3,8 +3,10 @@ package com.dotori.v2.domain.music.presentation.councillor
 import com.dotori.v2.domain.music.presentation.data.req.ApplyMusicReqDto
 import com.dotori.v2.domain.music.presentation.data.res.MusicListResDto
 import com.dotori.v2.domain.music.service.ApplyMusicService
+import com.dotori.v2.domain.music.service.DeleteMusicLikeService
 import com.dotori.v2.domain.music.service.DeleteMusicService
 import com.dotori.v2.domain.music.service.FindMusicsService
+import com.dotori.v2.domain.music.service.PostMusicLikeService
 import org.springframework.format.annotation.DateTimeFormat
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
@@ -17,7 +19,9 @@ import java.time.LocalDateTime
 class CouncillorMusicController(
     private val applyMusicService: ApplyMusicService,
     private val findMusicsService: FindMusicsService,
-    private val deleteMusicService: DeleteMusicService
+    private val deleteMusicService: DeleteMusicService,
+    private val postMusicLikeService: PostMusicLikeService,
+    private val deleteMusicLikeService: DeleteMusicLikeService,
 ) {
     @PostMapping
     fun applyMusic(@RequestBody applyMusicReqDto: ApplyMusicReqDto): ResponseEntity<Void> =
@@ -36,5 +40,15 @@ class CouncillorMusicController(
     @DeleteMapping("/{music_id}")
     fun deleteMusic(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
         deleteMusicService.execute(musicId)
+            .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
+
+    @PostMapping("/{music_id}/like")
+    fun postMusicLike(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
+        postMusicLikeService.execute(musicId)
+            .run { ResponseEntity.status(HttpStatus.OK).build() }
+
+    @DeleteMapping("/{music_id}/like")
+    fun deleteMusicLike(@PathVariable("music_id") musicId: Long): ResponseEntity<Void> =
+        deleteMusicLikeService.execute(musicId)
             .run { ResponseEntity.status(HttpStatus.NO_CONTENT).build() }
 }
