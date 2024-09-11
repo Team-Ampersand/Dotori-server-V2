fe7a54d3b079bde44d02e2c7969028f6daab0e19
ta2ye0n <tyeon42051@naver.com>
2024-09-11 16:56:56 +0900

    add :: NotValidMusicLikeException

---
+++ src/main/kotlin/com/dotori/v2/domain/music/exception/NotValidMusicLikeException.kt
@@ -0,0 +1,7 @@
+package com.dotori.v2.domain.music.exception
+
+import com.dotori.v2.global.error.ErrorCode
+import com.dotori.v2.global.error.exception.BasicException
+
+class NotValidMusicLikeException:BasicException(ErrorCode.NOT_VALID_MUSIC_LIKE) {
+}
