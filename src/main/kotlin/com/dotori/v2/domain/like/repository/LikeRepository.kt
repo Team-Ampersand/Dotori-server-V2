1841588de20823510f67e3bd8c670c192a33f9c2
ta2ye0n <tyeon42051@naver.com>
2024-09-12 19:42:02 +0900

    update :: 동시성 제어

--- src/main/kotlin/com/dotori/v2/domain/like/repository/LikeRepository.kt
+++ src/main/kotlin/com/dotori/v2/domain/like/repository/LikeRepository.kt
@@ -3,10 +3,14 @@ package com.dotori.v2.domain.like.repository
 import com.dotori.v2.domain.member.domain.entity.Member
 import com.dotori.v2.domain.like.entity.Like
 import com.dotori.v2.domain.music.domain.entity.Music
+import feign.Param
+import javax.persistence.LockModeType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Lock
+import org.springframework.data.jpa.repository.Query
 
-interface LikeRepository : JpaRepository<Like,Long> {
-    fun findByMember(member: Member): List<Like>
-
-    fun findByMemberAndMusic(member: Member,music: Music): Like
+interface LikeRepository : JpaRepository<Like, Long> {
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("select l from Like l where l.music = :music and l.member = :member")
+    fun findByMemberAndMusic(@Param("member") member: Member, @Param("music") music: Music): Like?
 }
