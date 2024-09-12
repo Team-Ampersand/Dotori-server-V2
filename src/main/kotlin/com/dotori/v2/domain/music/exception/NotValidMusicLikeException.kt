945b2a1181034a231c65650a0c45772652bdbd8c
태연 <132069501+ta2ye0n@users.noreply.github.com>
2024-09-12 20:02:32 +0900

    Update src/main/kotlin/com/dotori/v2/domain/music/exception/NotValidMusicLikeException.kt
    Co-authored-by: Hope Kim <piyrw9754@gmail.com>

--- src/main/kotlin/com/dotori/v2/domain/music/exception/NotValidMusicLikeException.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/exception/NotValidMusicLikeException.kt
@@ -3,5 +3,5 @@ package com.dotori.v2.domain.music.exception
 import com.dotori.v2.global.error.ErrorCode
 import com.dotori.v2.global.error.exception.BasicException
 
-class NotValidMusicLikeException:BasicException(ErrorCode.NOT_VALID_MUSIC_LIKE) {
+class NotValidMusicLikeException : BasicException(ErrorCode.NOT_VALID_MUSIC_LIKE) {
 }
