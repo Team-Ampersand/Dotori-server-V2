b3047e8d1f2302e2ff54724e516f13f804313179
uuuuuuuk <s22034@gsm.hs.kr>
2024-09-23 19:23:06 +0900

    update :: 프로필 설정 관련 기능에 캐시 로직 추가

--- src/main/kotlin/com/dotori/v2/domain/member/service/impl/DeleteProfileImageServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/member/service/impl/DeleteProfileImageServiceImpl.kt
@@ -1,6 +1,8 @@
 package com.dotori.v2.domain.member.service.impl
 
 import com.dotori.v2.domain.member.service.DeleteProfileImageService
+import com.dotori.v2.domain.student.presentation.data.res.FindAllStudentListResDto
+import com.dotori.v2.global.config.redis.service.RedisCacheService
 import com.dotori.v2.global.thirdparty.aws.s3.S3Service
 import com.dotori.v2.global.util.UserUtil
 import org.springframework.stereotype.Service
@@ -10,13 +12,25 @@ import org.springframework.transaction.annotation.Transactional
 @Transactional(rollbackFor = [Exception::class])
 class DeleteProfileImageServiceImpl(
     private val userUtil: UserUtil,
-    private val s3Service: S3Service
+    private val s3Service: S3Service,
+    private val redisCacheService: RedisCacheService
 ) : DeleteProfileImageService {
+
+    private val CACHE_KEY = "memberList"
 
     override fun execute() {
         val member = userUtil.fetchCurrentUser()
         s3Service.deleteFile(member.profileImage!!)
         member.updateProfileImage(null)
+        initCache()
     }
 
+    private fun initCache() {
+        val cachedData =
+            redisCacheService.getFromCache(CACHE_KEY) as? FindAllStudentListResDto
+
+        if (cachedData != null) {
+            redisCacheService.deleteFromCache(CACHE_KEY)
+        }
+    }
 }
