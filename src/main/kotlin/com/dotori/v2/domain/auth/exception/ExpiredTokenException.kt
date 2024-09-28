517a501d872a06908946292ff0f1e6db53b9c473
m2ri1 <parkmiri0722@gmail.com>
2024-09-29 01:27:11 +0900

    delete :: GAuth ErrorCode

--- src/main/kotlin/com/dotori/v2/domain/auth/exception/ExpiredTokenException.kt
+++
@@ -1,7 +0,0 @@
-package com.dotori.v2.domain.auth.exception
-
-import com.dotori.v2.global.error.ErrorCode
-import com.dotori.v2.global.error.exception.BasicException
-
-class ExpiredTokenException : BasicException(ErrorCode.EXPIRED_CODE) {
-}
--- src/main/kotlin/com/dotori/v2/domain/auth/exception/ServiceNotFoundException.kt
+++
@@ -1,7 +0,0 @@
-package com.dotori.v2.domain.auth.exception
-
-import com.dotori.v2.global.error.ErrorCode
-import com.dotori.v2.global.error.exception.BasicException
-
-class ServiceNotFoundException : BasicException(ErrorCode.SERVICE_NOT_FOUND) {
-}
