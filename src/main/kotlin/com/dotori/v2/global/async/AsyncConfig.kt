2ad4e37b5ba7afdaccc0b279450513e3c301eaed
esperar <piyrw9754@gmail.com>
2024-11-21 10:20:09 +0900

    update :: squirrel task executor

--- src/main/kotlin/com/dotori/v2/global/async/AsyncConfig.kt
+++ src/main/kotlin/com/dotori/v2/global/async/AsyncConfig.kt
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.annotation.EnableAsync
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 
-
 @EnableAsync
 @Configuration
 class AsyncConfig {
@@ -21,3 +20,4 @@ class AsyncConfig {
         return executor
     }
 }
+
