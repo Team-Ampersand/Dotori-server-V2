8db91f14dcbb7fcbdbc70eea930800b457a28b25
esperar <piyrw9754@gmail.com>
2024-12-04 08:24:13 +0900

    music like event publish

--- src/main/kotlin/com/dotori/v2/domain/music/service/impl/ToggleMusicLikeServiceImpl.kt
+++ src/main/kotlin/com/dotori/v2/domain/music/service/impl/ToggleMusicLikeServiceImpl.kt
@@ -9,10 +9,14 @@ import com.dotori.v2.domain.music.exception.MusicNotFoundException
 import com.dotori.v2.domain.music.presentation.data.res.MusicLikeCountResDto
 import com.dotori.v2.domain.music.service.ToggleMusicLikeService
 import com.dotori.v2.global.config.redis.service.RedisCacheService
+import com.dotori.v2.global.squirrel.MusicDotoriEvent
 import com.dotori.v2.global.util.UserUtil
+import org.springframework.context.ApplicationEventPublisher
+import org.springframework.core.env.Environment
 import org.springframework.transaction.annotation.Transactional
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
+import java.time.LocalDateTime
 
 @Service
 @Transactional(readOnly = false, rollbackFor = [Exception::class])
@@ -20,8 +24,11 @@ class ToggleMusicLikeServiceImpl(
     private val musicLikeRepository: MusicLikeRepository,
     private val musicRepository: MusicRepository,
     private val userUtil: UserUtil,
-    private val redisCacheService: RedisCacheService
+    private val redisCacheService: RedisCacheService,
+    private val applicationEventPublisher: ApplicationEventPublisher,
+    private val env: Environment
 ): ToggleMusicLikeService {
+
     override fun execute(musicId: Long) : MusicLikeCountResDto {
         val member = userUtil.fetchCurrentUser()
         val music: Music = musicRepository.findByIdOrNull(musicId) ?: throw MusicNotFoundException()
@@ -39,7 +46,7 @@ class ToggleMusicLikeServiceImpl(
         if (like == null) {
             saveLike(music, member)
         } else {
-            deleteLike(like.id, music)
+            deleteLike(like.id, music, member.memberName)
         }
     }
 
@@ -51,12 +58,28 @@ class ToggleMusicLikeServiceImpl(
         musicLikeRepository.save(musicLike)
         music.plusLikeCount()
         updateCache(music)
+        applicationEventPublisher.publishEvent(
+            MusicDotoriEvent.ofCreateMusicLikeEvent(
+                member.memberName,
+                createdAt = LocalDateTime.now(),
+                env = env.activeProfiles[0],
+                title = music.title
+            )
+        )
     }
 
-    private fun deleteLike (likeId: Long, music: Music) {
+    private fun deleteLike (likeId: Long, music: Music, memberName: String) {
         musicLikeRepository.deleteById(likeId)
         music.minusLikeCount()
         updateCache(music)
+        applicationEventPublisher.publishEvent(
+            MusicDotoriEvent.ofDeleteMusicLikeEvent(
+                memberName,
+                createdAt = LocalDateTime.now(),
+                env = env.activeProfiles[0],
+                title = music.title
+            )
+        )
     }
 
     private fun updateCache(music: Music) {
